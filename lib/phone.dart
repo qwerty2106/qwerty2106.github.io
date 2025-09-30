c09e701514f7d380189846ab86b702e8b156569f
Anna <imshrtlvv@yandex.ru>
2025-09-30 16:38:04 +0300

    functions have been added

--- lib/phone.dart
+++ lib/phone.dart
@@ -39,124 +39,117 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.all(5),
-              color: Colors.indigo,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(height: 50, width: 50, color: Colors.blueGrey),
-                      Container(height: 50, width: 100, color: Colors.blueGrey),
-                    ],
-                  ),
-
-                  Text('Total balance to spend'),
-                  Text(
-                    '5786.55',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  ),
-                ],
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.all(5),
-              color: Colors.grey,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Planning Ahead',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text('-540.52'),
-                    ],
-                  ),
-                  
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.all(5),
-                          height: 50,
-                          width: 100,
-                          color: Colors.blueGrey,
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(5),
-                          height: 50,
-                          width: 100,
-                          color: Colors.blueGrey,
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(5),
-                          height: 50,
-                          width: 100,
-                          color: Colors.blueGrey,
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(5),
-                          height: 50,
-                          width: 100,
-                          color: Colors.blueGrey,
-                        ),
-                      ],
-                    ),
-                  ),             
-                ],
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.all(5),
-              color: Colors.grey,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text('Last Month Expense'), Text('1800.50')],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Row(children: [Text('25')]),
-                          Row(children: [Text('MAR')]),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Row(children: [Text('26')]),
-                          Row(children: [Text('MAR')]),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Row(children: [Text('27')]),
-                          Row(children: [Text('MAR')]),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(color: Colors.indigo, child: header()),
+              body(),
+              footer(),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget header() {
+  return Container(
+    padding: EdgeInsets.all(5),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(height: 50, width: 50, color: Colors.white),
+            Container(height: 50, width: 100, color: Colors.white),
+          ],
+        ),
+
+        Text('Total balance to spend'),
+        Text(
+          '5786.55',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget body() {
+  return Container(
+    margin: EdgeInsets.all(5),
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Planning Ahead',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            Text('-540.52'),
+          ],
+        ),
+        scrollContainer(),
+      ],
+    ),
+  );
+}
+
+Widget scrollContainer() {
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: [
+        for (int i = 0; i < 15; i++)
+          Container(
+            margin: EdgeInsets.all(5),
+            height: 50,
+            width: 100,
+            color: Colors.indigo,
+          ),
+      ],
+    ),
+  );
+}
+
+Widget footer() {
+  return Container(
+    margin: EdgeInsets.all(5),
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text('Last Month Expense'), Text('1800.50')],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Row(children: [Text('25')]),
+                Row(children: [Text('MAR')]),
+              ],
+            ),
+            Column(
+              children: [
+                Row(children: [Text('26')]),
+                Row(children: [Text('MAR')]),
+              ],
+            ),
+            Column(
+              children: [
+                Row(children: [Text('27')]),
+                Row(children: [Text('MAR')]),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
